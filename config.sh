8df5f97f4afa87ccca50db1c054203c4a933f5e0
Christian Herzog <daduke@phys.ethz.ch>
2018-07-10 13:19:09 +0200

    ignore whitespaces in config.sh

--- config.sh
+++ config.sh
@@ -3,14 +3,14 @@
 source config.ini
 
 #configure Python
-sed -E -i "s#(XYMONCLI = ')(.+?)(')#\1$XYMONCLI\3#" cgi/xymon-ack
-sed -E -i "s#(XYMONCLI = ')(.+?)(')#\1$XYMONCLI\3#" cgi/xymon2json
-sed -E -i "s#(CRITICAL = ')(.+?)(')#\1$CRITICAL\3#" cgi/xymon2json
+sed -E -i "s#(XYMONCLI += ')(.+?)(')#\1$XYMONCLI\3#" cgi/xymon-ack
+sed -E -i "s#(XYMONCLI += ')(.+?)(')#\1$XYMONCLI\3#" cgi/xymon2json
+sed -E -i "s#(CRITICAL += ')(.+?)(')#\1$CRITICAL\3#" cgi/xymon2json
 
 #configure JS
-sed -E -i "s#(XYMONURL     = ')(.+?)(')#\1$XYMONURL\3#" js/main.js
-sed -E -i "s#(XYMONACKURL  = ')(.+?)(')#\1$XYMONACKURL\3#" js/main.js
-sed -E -i "s#(XYMONJSONURL = ')(.+?)(')#\1$XYMONJSONURL\3#" js/main.js
+sed -E -i "s#(XYMONURL += ')(.+?)(')#\1$XYMONURL\3#" js/main.js
+sed -E -i "s#(XYMONACKURL += ')(.+?)(')#\1$XYMONACKURL\3#" js/main.js
+sed -E -i "s#(XYMONJSONURL += ')(.+?)(')#\1$XYMONJSONURL\3#" js/main.js
 
 #configure HTML
 sed -E -i "s#(<title>)(.+?)(</title>)#\1$TITLE\3#" index.html
