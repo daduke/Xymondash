8cbfcfaadda119ff4049e0db83dbdfe79d039158
Iain <iain@boldfield.com>
2019-04-29 15:11:46 +0100

    Don't use 0 as the server use XYMONSERVER

--- config.sh
+++ config.sh
@@ -3,6 +3,8 @@
 source config.ini
 
 #configure Python
+sed -E -i "s#(XYMONSERVER += ')(.+?)(')#\1$XYMONSERVER\3#" cgi/xymon-ack
+sed -E -i "s#(XYMONSERVER += ')(.+?)(')#\1$XYMONSERVER\3#" cgi/xymon-disable
 sed -E -i "s#(XYMONCLI += ')(.+?)(')#\1$XYMONCLI\3#" cgi/xymon-ack
 sed -E -i "s#(XYMONCLI += ')(.+?)(')#\1$XYMONCLI\3#" cgi/xymon-disable
 sed -E -i "s#(XYMONACKINFOSH += ')(.+?)(')#\1$XYMONACKINFOSH\3#" cgi/xymon-ack
@@ -22,4 +24,4 @@ sed -E -i "s#(XYMONSERVER += ')(.+?)(')#\1$XYMONSERVER\3#" js/main.js
 
 #configure HTML
 sed -E -i "s#(<title>)(.+?)(</title>)#\1$TITLE\3#" index.html
-sed -E -i "s#(<h1 class=\"text-white\" id=\"t\">)(.+?)(</h1>)#\1$TITLE\3#" index.html
+sed -E -i "s#(<h1 class=\"text-white\" id=\"t\">)(.+?)(</h1>)#\1<A class=\"text-white\" href=\"$TITLELINK\">$TITLE</A>\3#" index.html
